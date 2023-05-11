30f485fe78f081c3d58f5ea796c3ecaff65f7a2b
RikVandermullen <rik.vandermullen@gmail.com>
2023-05-11 20:02:00 +0200

    Added webRTC

--- webserver/index.js
+++ webserver/index.js
@@ -1,4 +1,12 @@
-var title = document.getElementById("title");
-title.addEventListener("click", function () {
-	title.innerHTML = "This is a JS file";
-});
+async function playVideoFromCamera() {
+	try {
+		const constraints = { video: true, audio: true };
+		const stream = await navigator.mediaDevices.getUserMedia(constraints);
+		const videoElement = document.querySelector("video#localVideo");
+		videoElement.srcObject = stream;
+	} catch (error) {
+		console.error("Error opening video camera.", error);
+	}
+}
+
+playVideoFromCamera();
